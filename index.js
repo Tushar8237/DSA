be33be93add23a8a4d08b3cc932b7db9121d1e2c
Tushar Suryawanshi <tushar.823737@gmail.com>
2022-06-14 20:48:37 +0530

    DSA

--- index.js
+++ index.js
@@ -96,25 +96,25 @@
 //  Find the index of given no in a arry 7
 //  [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15] ==> index 6 ->output
 
-function searchAlog(array, number) {
-  let min = 0;
-  let max = array.lenght - 1;
-  while (min <= max) {
-    let midIndex = Math.floor((min + max) / 2);
-    if (array[midIndex] < number) {
-      min = midIndex + 1;
-    } else if (array[midIndex] > number) {
-      max = midIndex - 1;
-    } else {
-      return midIndex;
-    }
-  }
-  return -1;
-}
+// function searchAlog(array, number) {
+//   let min = 0;
+//   let max = array.lenght - 1;
+//   while (min <= max) {
+//     let midIndex = Math.floor((min + max) / 2);
+//     if (array[midIndex] < number) {
+//       min = midIndex + 1;
+//     } else if (array[midIndex] > number) {
+//       max = midIndex - 1;
+//     } else {
+//       return midIndex;
+//     }
+//   }
+//   return -1;
+// }
 
-const result = searchAlog(
-  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-  7
-);
+// const result = searchAlog(
+//   [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+//   7
+// );
 
-console.log(result);
+// console.log(result);
