f110c53f217310c93619be655ec9c7053d91689c
Tushar Suryawanshi <tushar.823737@gmail.com>
2022-06-15 00:55:34 +0530

    array

---
+++ mock.js
@@ -0,0 +1,20 @@
+//  remove duplicate item from the array
+// var array = [1, 1, 2, 3, 4, 5, 2, 3, 6, 7, 8];
+
+// let array = ["tushar", "anil", "pradeep", "tushar", "gaurav", "anil"];
+
+// function duplicate(array) {
+//   return array.filter((item, index) => array.indexOf(item) === index);
+// }
+
+// console.log(duplicate(array));
+// const result = duplicate(array);
+
+// console.log(result);
+
+// array reverse
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// let reverse = array.reverse();
+
+// console.log(reverse);
