f9f095cdb090d8cfc0e4dad04cec7ec1de5e8677
Tushar Suryawanshi <tushar.823737@gmail.com>
2022-06-23 21:33:14 +0530

    DSA

--- index.js
+++ index.js
@@ -259,3 +259,54 @@
 // };
 
 // console.log(maxRepeating(str));
+
+//  Longest Increasing Subsequence
+
+let max_ref;
+
+function _lis(arr, n) {
+  // base case
+  if (n == 1) return 1;
+  // maxEndingHere is the LIS ending with arr [n-1]
+  let res,
+    max_ending_here = 1;
+
+  /* Recursively get all LIS ending with arr[0], arr[1] ...
+           arr[n-2]. If   arr[i-1] is smaller than arr[n-1], and
+           max ending with arr[n-1] needs to be updated, then
+           update it */
+
+  for (let i = 1; i < n; i++) {
+    res = _lis(arr, i);
+    if (arr[i - 1] < arr[n - 1] && res + 1 > max_ending_here)
+      max_ending_here = res + 1;
+  }
+
+  // Compare max_ending_here with the overall max. And
+  // update the overall max if needed
+
+  if (max_ref < max_ending_here) max_ref = max_ending_here;
+
+  // Return lenght of LIS ending with arr[n_1]
+  return max_ending_here;
+}
+
+//  The wrapper function for _lis()
+
+function lis(arr, n) {
+  // The max variable holds the result
+  max_ref = 1;
+
+  // The function _lis() stores its result in max
+  _lis(arr, n);
+
+  // returns max
+  return max_ref;
+}
+
+// driver program to test above functions
+
+let arr = [10, 22, 9, 33, 21, 50, 41, 60];
+let n = arr.length;
+
+console.log("Lenght of lis is " + lis(arr, n) + " " + "<br>");
