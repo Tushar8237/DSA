f0f9dd68318c5d6cc4e824aa4f437ccca4ef2a7b
Tushar Suryawanshi <tushar.823737@gmail.com>
2022-06-24 23:51:29 +0530

    array

--- arrays.js
+++ arrays.js
@@ -144,3 +144,24 @@
 // const result = findMinmum([2, 3, 4, 5, 6, 8]);
 
 // console.log(result);
+
+function checkIfSquareExits(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    let isSquare = false;
+    for (let j = 0; j < array1.length; j++) {
+      if (array1[i] * array1[i] === array2[j]) {
+        isSquare = true;
+      }
+      if (j === array2.length - 1) {
+        if (!isSquare) {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+}
+
+const result = checkIfSquareExits([1, 2, 3, 4], [1, 4, 9, 16]);
+
+console.log(result);
