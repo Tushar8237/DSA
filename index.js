05d46579b9629d8dbd737e2f092d0e0efff3da4c
Tushar Suryawanshi <tushar.823737@gmail.com>
2022-06-27 20:44:48 +0530

    valid parenthesis

--- index.js
+++ index.js
@@ -262,51 +262,90 @@
 
 //  Longest Increasing Subsequence
 
-let max_ref;
+// let max_ref;
 
-function _lis(arr, n) {
-  // base case
-  if (n == 1) return 1;
-  // maxEndingHere is the LIS ending with arr [n-1]
-  let res,
-    max_ending_here = 1;
+// function _lis(arr, n) {
+// base case
+// if (n == 1) return 1;
+// maxEndingHere is the LIS ending with arr [n-1]
+// let res,
+// max_ending_here = 1;
 
-  /* Recursively get all LIS ending with arr[0], arr[1] ...
+/* Recursively get all LIS ending with arr[0], arr[1] ...
            arr[n-2]. If   arr[i-1] is smaller than arr[n-1], and
            max ending with arr[n-1] needs to be updated, then
            update it */
 
-  for (let i = 1; i < n; i++) {
-    res = _lis(arr, i);
-    if (arr[i - 1] < arr[n - 1] && res + 1 > max_ending_here)
-      max_ending_here = res + 1;
-  }
+// for (let i = 1; i < n; i++) {
+//   res = _lis(arr, i);
+//   if (arr[i - 1] < arr[n - 1] && res + 1 > max_ending_here)
+//     max_ending_here = res + 1;
+// }
 
-  // Compare max_ending_here with the overall max. And
-  // update the overall max if needed
+// Compare max_ending_here with the overall max. And
+// update the overall max if needed
 
-  if (max_ref < max_ending_here) max_ref = max_ending_here;
+// if (max_ref < max_ending_here) max_ref = max_ending_here;
 
-  // Return lenght of LIS ending with arr[n_1]
-  return max_ending_here;
-}
+// Return lenght of LIS ending with arr[n_1]
+// return max_ending_here;
+// }
 
 //  The wrapper function for _lis()
 
-function lis(arr, n) {
-  // The max variable holds the result
-  max_ref = 1;
+// function lis(arr, n) {
+// The max variable holds the result
+// max_ref = 1;
 
-  // The function _lis() stores its result in max
-  _lis(arr, n);
+// The function _lis() stores its result in max
+// _lis(arr, n);
 
-  // returns max
-  return max_ref;
-}
+// returns max
+// return max_ref;
+// }
 
 // driver program to test above functions
 
-let arr = [10, 22, 9, 33, 21, 50, 41, 60];
-let n = arr.length;
+// let arr = [10, 22, 9, 33, 21, 50, 41, 60];
+// let n = arr.length;
 
-console.log("Lenght of lis is " + lis(arr, n) + " " + "<br>");
+// console.log("Lenght of lis is " + lis(arr, n) + " " + "<br>");
+
+// Check if given string Parentheses expression is balnced or not
+
+/*Input: str = “((()))()()” 
+Output: Balanced
+Input: str = “())((())” 
+Output: Not Balanced  */
+
+// Function to check if parenthesis are balanced
+
+function isBalanced(exp) {
+  // initailising variables
+  let flag = true;
+  let count = 0;
+
+  // traversing the expression
+  for (let i = 0; i < exp.length; i++) {
+    if (exp[i] == "(") count += 1;
+    // it is closing parenthesis
+    else count -= 1;
+
+    if (count < 0) {
+      // this means there are more closing parenthesis than opening
+      flag = false;
+      break;
+    }
+  }
+
+  // if count is not zero it means there are more opening parenthesis
+  if (count != 0) flag = false;
+
+  return flag;
+}
+
+let exp = "((()))()()";
+// let exp1 = "((()))()())";
+
+if (isBalanced(exp)) console.log("Balanced");
+else console.log("Not Balanced");
