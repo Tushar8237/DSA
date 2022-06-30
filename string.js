94ef51122b9dbfe30a39db26b7c732cac8d338d1
Tushar Suryawanshi <tushar.823737@gmail.com>
2022-06-30 18:16:01 +0530

    string

--- string.js
+++ string.js
@@ -42,16 +42,55 @@
 
 // count all those name start with a
 
-let name = ["tushar", "roshan", "rajesh", "ankush", "anil", "pradeep", "Akash"];
-let nameWithA = [];
-let count = 0;
-for (let i = 0; i < name.length; i++) {
-  let current_name = name[i];
+// let name = ["tushar", "roshan", "rajesh", "ankush", "anil", "pradeep", "Akash"];
+// let nameWithA = [];
+// let count = 0;
+// for (let i = 0; i < name.length; i++) {
+//   let current_name = name[i];
 
-  if (current_name[0] == "a" || current_name[0] == "A") {
-    count++;
-    nameWithA.push(current_name);
-  }
-}
-console.log(count);
-console.log(nameWithA);
+//   if (current_name[0] == "a" || current_name[0] == "A") {
+//     count++;
+//     nameWithA.push(current_name);
+//   }
+// }
+// console.log(count);
+// console.log(nameWithA);
+
+// count all those name which have a in them
+
+// let name = ["tushar", "roshan", "rajesh", "ankush", "anil", "pradeep", "Akash"];
+// let count = [];
+// for (let i = 0; i < name.length; i++) {
+//   let current_name = name[i];
+
+//   for (let j = 0; j < current_name.length; j++) {
+//     if (current_name[j] == "a" || current_name[j] == "A") {
+//       count++;
+//       break;
+//     }
+//   }
+// }
+
+// console.log(count);
+
+// given string count the number of words
+
+// "there is a need development in the aducation system"
+
+// let str = "there is a need development in the aducation system";
+// // let str = "";
+
+// let count = 0;
+
+// if (str.length > 0) {
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] == " ") {
+//       count++;
+//     }
+//   }
+//   count = count + 1;
+// } else {
+//   console.log("no words in the string");
+// }
+
+// console.log(count);
