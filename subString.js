34b5fb5cf07c2112ad1721273d027925ff3c3d73
Tushar Suryawanshi <tushar.823737@gmail.com>
2022-07-03 13:39:05 +0530

    string

--- subString.js
+++ subString.js
@@ -1,26 +1,30 @@
 // you are given a string "s" you have to find the "count" of substring which start and end with the same character
 // sample input "abcab"
 // sample output 7
-
 // 1 Generate all possible substring
 // 2 check for all generated substring if the first and last character are same then increment the count
 
-let str = "abcab";
-let n = str.length;
-let count = 0;
+const countSubString = () => {
+  let str = "abcab";
+  let n = str.length;
+  let count = 0;
 
-for (let i = 0; i < n; i++) {
-  for (let j = i; j < n; j++) {
-    let bag = "";
-    for (let k = i; k <= j; k++) {
-      bag = bag + str[k];
-    }
-    var n2 = bag.length - 1;
-    if (bag[0] == bag[n2]) {
-      count++;
-      console.log(bag);
+  for (let i = 0; i < n; i++) {
+    for (let j = i; j < n; j++) {
+      let bag = "";
+      for (let k = i; k <= j; k++) {
+        bag = bag + str[k];
+      }
+      var n2 = bag.length - 1;
+      if (bag[0] == bag[n2]) {
+        count++;
+        console.log(bag);
+      }
     }
   }
-}
+  return count;
+};
 
-console.log("count is ", count);
+let result = countSubString();
+
+console.log("count is", result);
