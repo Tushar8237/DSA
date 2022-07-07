98a3627affd52a8299a278885777618bd33a5906
Tushar Suryawanshi <tushar.823737@gmail.com>
2022-07-08 03:01:00 +0530

    pattern

---
+++ patternQue.js
@@ -0,0 +1,33 @@
+for (let j = 1; j <= 5; j++) {
+  let bag = "";
+
+  for (let i = 0; i < 5; i++) {
+    bag = bag + " * ";
+  }
+  console.log(bag);
+}
+
+for (let i = 1; i <= 5; i++) {
+  var bag = "";
+
+  for (let j = 1; j <= i; j++) {
+    bag = bag + "  " + j;
+  }
+  console.log(bag);
+}
+
+for (let i = 1; i <= 5; i++) {
+  var bag = "";
+  for (let j = 1; j <= i; j++) {
+    bag = bag + " * ";
+  }
+  console.log(bag);
+}
+
+for (let i = 1; i <= 5; i++) {
+  var bag = "";
+  for (let j = 1; j <= i; j++) {
+    bag = bag + "  " + j;
+  }
+  console.log(bag);
+}
