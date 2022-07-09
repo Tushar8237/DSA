245299c1f639f4da7274e5f462544f481d838c0c
Tushar Suryawanshi <tushar.823737@gmail.com>
2022-07-09 15:01:51 +0530

    patternQ

--- patternQue.js
+++ patternQue.js
@@ -7,22 +7,22 @@
 //   console.log(bag);
 // }
 
-// for (let i = 1; i <= 5; i++) {
-//   var bag = "";
+for (let i = 1; i <= 5; i++) {
+  var bag = "";
 
-//   for (let j = 1; j <= i; j++) {
-//     bag = bag + "  " + j;
-//   }
-//   console.log(bag);
-// }
+  for (let j = 1; j <= i; j++) {
+    bag = bag + "  " + j;
+  }
+  console.log(bag);
+}
 
-// for (let i = 1; i <= 5; i++) {
-//   var bag = "";
-//   for (let j = 1; j <= i; j++) {
-//     bag = bag + " * ";
-//   }
-//   console.log(bag);
-// }
+for (let i = 1; i <= 5; i++) {
+  var bag = "";
+  for (let j = 1; j <= i; j++) {
+    bag = bag + " * ";
+  }
+  console.log(bag);
+}
 
 // for (let i = 1; i <= 5; i++) {
 //   var bag = "";
@@ -59,6 +59,45 @@ for (let row = 1; row <= 10; row++) {
     } else {
       if (col == 1 || col == 10) {
         bag = bag + " * ";
+      } else {
+        bag = bag + " ";
+      }
+    }
+  }
+  console.log(bag);
+}
+
+for (let i = 0; i <= 6; i++) {
+  let bag = "";
+  // printing space
+  for (let j = 0; j <= 6 - i; j++) {
+    // bag += " ";
+    bag = bag + " ";
+  }
+  // printing star
+  for (let k = 0; k < i; k++) {
+    // bag += "*";
+    bag = bag + "*";
+  }
+  console.log(bag);
+}
+
+for (let i = 0; i < 6; i++) {
+  let bag = "";
+  for (let j = 0; j < 6 - i; j++) {
+    bag = bag + "*";
+  }
+  console.log(bag);
+}
+
+for (let i = 1; i <= 6; i++) {
+  let bag = "";
+  for (let j = 0; j < i; j++) {
+    if (i === 6) {
+      bag = bag + "*";
+    } else {
+      if (j == 0 || j == i - 1) {
+        bag = bag + "*";
       } else {
         bag = bag + " ";
       }
