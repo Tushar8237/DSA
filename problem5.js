aa77260e6f45b7f6672b04081bf580dd8aa42fb5
Tushar Suryawanshi <tushar.823737@gmail.com>
2022-07-10 22:20:29 +0530

    problem

---
+++ problem5.js
@@ -0,0 +1,20 @@
+function processData(input) {
+  var v = input.split(" ");
+  for (var i = 0; i < v.length; i++) {
+    v[i] = parseInt(v[i]);
+  }
+  var max = -Infinity;
+  var min = Infinity;
+  for (var i = 0; i < v.length; i++) {
+    var sum = 0;
+    for (var j = 0; j < v.length; j++) {
+      if (i != j) {
+        sum += v[j];
+      }
+    }
+    if (sum < min) min = sum;
+    if (sum > max) max = sum;
+  }
+  console.log(min, max);
+}
+processData([1, 2, 3, 4, 5]);
