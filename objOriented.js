18e55957623e0b3909535cc5778f07337c87b71c
Tushar Suryawanshi <tushar.823737@gmail.com>
2022-07-17 02:16:23 +0530

    clase

--- objOriented.js
+++ objOriented.js
@@ -51,3 +51,46 @@ me.name = "Mukul";
 me.isStudying = true;
 
 me.introduction();
+
+// Class
+/* Classes - classes are blueprint of an object. A class can have many object because class is a templete while object are instances of the class or the concrete implementation */
+
+// Defining class using es6
+
+class Vehicle {
+  constructor(name, maker, engine) {
+    this.name = name;
+    this.maker = maker;
+    this.engine = engine;
+  }
+  getDetails() {
+    return `The name of the bike is ${this.name}`;
+  }
+}
+
+let bike1 = new Vehicle("Hayabusa", "Suzuki", "1340cc");
+let bike2 = new Vehicle("Ninja", "Kawasaki", "998cc");
+
+console.log(bike1.name, bike1.maker, bike1.engine);
+console.log(bike2.maker);
+console.log(bike2.getDetails());
+
+// Traditional way
+
+// Defing class in a traditional way
+function Vehicle1(name, maker, engine) {
+  this.name = name;
+  this.maker = maker;
+  this.engine = engine;
+}
+
+Vehicle1.prototype.getDetail = function () {
+  console.log("The name of the bike is " + this.name);
+};
+
+let bike3 = new Vehicle1("Appache RTR", "TVS", "160cc");
+let bike4 = new Vehicle1("Activa", "Honda", "125cc");
+
+console.log(bike3.name);
+console.log(bike4.maker, bike4.maker, bike4.engine);
+console.log(bike3.getDetail());
